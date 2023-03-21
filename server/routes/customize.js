985a93c72c1303289192b61be31f45acd53dcdde
= <=>
2023-03-21 14:30:11 +0700

    full project

---
+++ server/routes/customize.js
@@ -0,0 +1,27 @@
+// file này dùng để quản lý slide ảnh chạy trong trang chủ
+const express = require("express");
+const router = express.Router();
+const customizeController = require("../controller/customize");
+const multer = require("multer");
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads/customize");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "_" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.get("/get-slide-image", customizeController.getImages);
+router.post("/delete-slide-image", customizeController.deleteSlideImage);
+router.post(
+  "/upload-slide-image",
+  upload.single("image"),
+  customizeController.uploadSlideImage
+);
+router.post("/dashboard-data", customizeController.getAllData);
+
+module.exports = router;
